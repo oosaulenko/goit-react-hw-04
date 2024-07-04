5b0260c93daf9993803319d1672fac0c209ad1f3
Oleh Osaulenko <oleh.osaulenko@vista.com>
2024-07-04 12:19:10 +0300

    Init commit

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function fetchList(query, page = 1){
-    return await axios.get("https://api.unsplash.com/search/pho1tos", {
+    return await axios.get("https://api.unsplash.com/search/photos", {
         params: {
             query,
             page,
